6df3a0253b253df91fcf272eba4e889cb53bcff3
rodovalhovic <victorialicia22@gmail.com>
2024-04-18 10:44:45 -0300

    subscribe (com tratamento de erro) e unsubscribe

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnDestroy {
 
   listaLivros: [];
+  campoBusca: string = '';
+  subscription: Subscription;
 
-  constructor() { }
+  constructor(private service: LivroService) { }
 
+  buscarLivros() {
+    this.subscription = this.service.buscar(this.campoBusca).subscribe({
+        next: retornoAPI => console.log(retornoAPI),
+        error: erro => console.error(erro)
+    })
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
 }
 
 
