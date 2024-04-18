73a51a6aae1ed74cd2a1f4f022b52dab3f5afbf9
rodovalhovic <victorialicia22@gmail.com>
2024-04-18 11:38:04 -0300

    map para filtrar os valores necessários na busca

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -1,6 +1,7 @@
+import { Item } from './../models/interfaces';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
 import { LivrosResultado } from '../models/interfaces';
 
 @Injectable({
@@ -11,10 +12,10 @@ export class LivroService {
   private readonly API = 'https://www.googleapis.com/books/v1/volumes'
   constructor(private http: HttpClient) { }
 
-  buscar(valorDigitado: string): Observable<LivrosResultado> {
+  buscar(valorDigitado: string): Observable<Item[]> {
     const params = new HttpParams().append('q', valorDigitado )
     return this.http.get<LivrosResultado>(this.API, { params }).pipe(
-      tap((retornoAPI) => console.log(retornoAPI))
+      map(resultado => resultado.items),
     )
   }
 }
